a8c644f20d21f0e057aa3fa90ebcdd6b2604e15d
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-31 23:52:15 +0700

    Update Cart Component

--- frontend/src/components/CardComponent.jsx
+++ frontend/src/components/CardComponent.jsx
@@ -2,12 +2,22 @@ import { Card, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 const CardComponent = ({ product, setCart }) => {
+  if (!product) return null; // Hindari error jika product tidak tersedia
+
   return (
     <Col md={4} xs={6} className="mb-4">
-      <Card className="shadow" onClick={() => setCart(product)}>
-        <Card.Img variant="top" src={"/img/" + product.image} />
+      <Card
+        className="shadow"
+        onClick={() => setCart(product)}
+        style={{ cursor: "pointer" }} // Menambahkan cursor pointer
+      >
+        <Card.Img
+          variant="top"
+          src={`/img/${product.image}`}
+          alt={product.name} // Tambahkan alt untuk aksesibilitas
+        />
         <Card.Body>
-          <Card.Title>{product.name + " (" + product.code + ")"}</Card.Title>
+          <Card.Title>{`${product.name} (${product.code})`}</Card.Title>
           <Card.Text>Rp. {product.price.toLocaleString("id-ID")}</Card.Text>
         </Card.Body>
       </Card>
@@ -16,8 +26,13 @@ const CardComponent = ({ product, setCart }) => {
 };
 
 CardComponent.propTypes = {
-  product: PropTypes.object,
-  setCart: PropTypes.func,
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    code: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  setCart: PropTypes.func.isRequired,
 };
 
 export default CardComponent;
