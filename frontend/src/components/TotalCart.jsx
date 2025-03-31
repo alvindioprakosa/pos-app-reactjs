55f328d2679aab0a137b62e1b92fb62626fae4ad
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-31 23:52:44 +0700

    Update Total Chart

--- frontend/src/components/TotalCart.jsx
+++ frontend/src/components/TotalCart.jsx
@@ -4,23 +4,29 @@ import { FaCartArrowDown } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { saveOrder } from "../features/CartSlice.js";
 import Swal from "sweetalert2";
-const TotalCart = ({ carts }) => {
-  const dispatch = useDispatch();
-  let sum = 0;
-  if (carts) {
-    sum = carts.reduce(function (result, item) {
-      return result + parseInt(item.totalPrice);
-    }, 0);
-  }
+import { useMemo } from "react";
 
-  const saveCartData = (data) => {
+const TotalCart = ({ carts = [] }) => {
+  const dispatch = useDispatch();
+
+  const sum = useMemo(
+    () => carts.reduce((total, item) => total + parseInt(item.totalPrice), 0),
+    [carts]
+  );
+
+  const saveCartData = () => {
     const orderData = {
-      date: new Date(),
+      date: new Date().toISOString(),
       total: sum,
-      detail: data,
+      detail: carts,
     };
+
     dispatch(saveOrder(orderData));
-    Swal.fire("Order Success!", "", "success");
+
+    Swal.fire("Order Success!", "", "success").then(() => {
+      console.log("Order berhasil disimpan!", orderData);
+      // Tambahkan navigasi atau reset state jika diperlukan di sini
+    });
   };
 
   return (
@@ -29,7 +35,7 @@ const TotalCart = ({ carts }) => {
         <Col md={{ span: 3, offset: 9 }} className="bg-body pt-2">
           <div className="px-3">
             <h4>
-              Total Bayar :{" "}
+              Total Bayar:{" "}
               <strong className="float-end me-3">
                 Rp {sum.toLocaleString("id-ID")}
               </strong>
@@ -38,7 +44,8 @@ const TotalCart = ({ carts }) => {
               variant="primary"
               className="w-100 me-3 mb-3"
               size="lg"
-              onClick={() => saveCartData(carts)}
+              onClick={saveCartData}
+              disabled={carts.length === 0} // Mencegah klik jika cart kosong
             >
               <FaCartArrowDown /> Bayar
             </Button>
